3e1a1504016310eedfde0ac8b50d809dd549c2f7
DevByDipto <dipto.1900@gmail.com>
2025-08-24 14:09:59 +0600

    practice how to create a basic server with express js

---
+++ class-3/dist/service.js
@@ -0,0 +1,10 @@
+import app from "./app.js";
+const port = 5000;
+let server;
+const boostrap = async () => {
+    server = app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
+    });
+};
+boostrap();
+//# sourceMappingURL=service.js.map
